ea93374e4f0763bff3f5c2981f4f3a21aa199617
Abdul Salam <abdulsalamsabil@gmail.com>
2021-08-30 20:10:58 +0700

    postgres database implemented

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,6 +1,6 @@
 const { nanoid } = require('nanoid');
-const InvariantError = require('../../exception/InvariantError');
-const NotFoundError = require('../../exception/NotFoundError');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
